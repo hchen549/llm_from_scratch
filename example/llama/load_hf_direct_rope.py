fcdf8b229cb9740dcbe0e6751df05388a50d301f
hchen549 <hchen549@wisc.edu>
2025-08-24 18:38:40 +0000

    refactor: standardize forward method signatures and clean up generation logic
    fix: unify argument order across all forward/fast_forward methods
    
    refactor: clean up llama3 model interface and remove debug prints

--- example/llama/load_hf_direct_rope.py
+++ example/llama/load_hf_direct_rope.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 from transformers import AutoModelForCausalLM, AutoConfig
 from transformers.models.llama.modeling_llama import LlamaRotaryEmbedding, apply_rotary_pos_emb
 from config import model_config
-from llm_basics.cs336_basics.model.llama3 import Llama3DirectHFRope
+from llm_basics.cs336_basics.model.llama3 import Llama3DirectHFRope, Llama3DirectHFRopeFastInference
 
 from llm_basics.cs336_basics.layer import TransformerBlock, RMSNorm, Linear, Embedding, scaled_dot_product_attention, SwiGLU
 import logging
@@ -14,17 +14,16 @@ from example.llama.load_hf import get_parameter_mapping, get_variable_mapping, t
 logging.basicConfig(level=logging.INFO)
 
 
-def load_and_test():
-    # Load HF model with eager attention for exact matching
+def load_model(hf_model_name, model_type = "llama3_hf_rope"):
+     # Load HF model with eager attention for exact matching
     hf_model = AutoModelForCausalLM.from_pretrained(
-        "meta-llama/Llama-3.2-1B-Instruct",
+        hf_model_name,
         torch_dtype=torch.bfloat16,
         attn_implementation="eager"
     ).cuda()
     
     # Get HF RoPE
     hf_rope = hf_model.model.rotary_emb
-    
     # Create custom config
     hf_config = hf_model.config
     my_model_cfg = model_config.LLamaConfig()
@@ -35,7 +34,12 @@ def load_and_test():
             setattr(my_model_cfg, value, getattr(hf_config, key))
     
     # Create custom model with HF RoPE
-    model = Llama3DirectHFRope(my_model_cfg, hf_rope)
+    if model_type == "llama3_hf_rope":
+        model = Llama3DirectHFRope(my_model_cfg, hf_rope)
+    elif model_type == "llama3_hf_rope_fast_inference":
+        model = Llama3DirectHFRopeFastInference(my_model_cfg, hf_rope)
+    else:
+        raise ValueError(f"Model type {model_type} not supported")
     model = model.to(torch.bfloat16).cuda()
     
     # Load weights
@@ -49,9 +53,14 @@ def load_and_test():
                 param.copy_(hf_state_dict[hf_param_name])
                 logging.info(f"Copied {hf_param_name} -> {name}")
     
+    return hf_model, model
+
+def load_and_test():
+    # load hf_model and my model
+    hf_model, model = load_model("meta-llama/Llama-3.2-1B-Instruct",)
+    
     # Test the model
     test_results = test_model(hf_model, model)
-    
     return hf_model, model, test_results
 
 
